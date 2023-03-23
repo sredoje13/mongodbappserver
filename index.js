8a2508de7d7f6e18dbc7e1d7c02ff683b5ff51dc
Jelena Popovic <popovicccjelena997@gmail.comm>
2023-03-23 19:17:31 +0100

    hell

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-const uri="mongodb+srv://jelenaaneww:jasamkralj@cluster0.esyhbj6.mongodb.net/?retryWrites=true&w=majority"
+const uri=process.env.MONGO_URI
 const PORT=process.env.PORT||5000;
 
 mongoose.connect(uri, { useNewUrlParser:true, useUnifiedTopology:true })
